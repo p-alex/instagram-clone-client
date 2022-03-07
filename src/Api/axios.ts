f9fd25f974c8a779cb8ce2f80d78f4ef4d1c0a46
p-alex <alexey9021@gmail.com>
2022-03-08 00:59:41 +0200

    Update axios base url

--- src/Api/axios.ts
+++ src/Api/axios.ts
@@ -1,5 +1,8 @@
 import axios from 'axios';
-const BASE_URL = 'http://localhost:5000/graphql';
+const BASE_URL =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:5000/graphql'
+    : 'https://instagram-clone-9021.herokuapp.com/graphql';
 
 export default axios.create({
   baseURL: BASE_URL,

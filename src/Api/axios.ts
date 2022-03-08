ed26a053dddda6cf4ce33e4eb59d52adc909a8c7
p-alex <alexey9021@gmail.com>
2022-03-08 21:09:13 +0200

    Update axios

--- src/Api/axios.ts
+++ src/Api/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const BASE_URL =
-  process.env.NODE_ENV === 'production'
+  process.env.NODE_ENV === 'development'
     ? 'http://localhost:5000/graphql'
     : 'https://instagram-clone-9021.herokuapp.com/graphql';
 

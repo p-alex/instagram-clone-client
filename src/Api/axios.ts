e1d089e222aff787a4a4f3dc98a48adc28d8055d
p-alex <alexey9021@gmail.com>
2022-03-08 21:06:50 +0200

    Fix netlify bug

--- src/Api/axios.ts
+++ src/Api/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const BASE_URL =
-  process.env.NODE_ENV === 'development'
+  process.env.NODE_ENV === 'production'
     ? 'http://localhost:5000/graphql'
     : 'https://instagram-clone-9021.herokuapp.com/graphql';
 

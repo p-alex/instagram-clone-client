2a5dac7fd89aa5a421258190c9fbb287b53f1be0
p-alex <alexey9021@gmail.com>
2022-03-17 09:54:11 +0200

    Update baseUrl

--- src/Util/baseURL.ts
+++ src/Util/baseURL.ts
@@ -1,4 +1,4 @@
 export const BASE_URL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:5001/graphql'
-    : 'https://instagram-clone-9021.herokuapp.com/graphql';
+    : 'https://bubble-9021.herokuapp.com/graphql';

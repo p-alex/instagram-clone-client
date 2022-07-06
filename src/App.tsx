0ff23b07717e221011bc634a16c195c6c82c2733
Alex Daniel <alexey9021@gmail.com>
2022-07-06 18:02:50 +0300

    Add code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,61 @@
+import { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import RequireAuth from "./Components/RequireAuth";
 import PersistLogin from "./Components/PersistLogin";
-import Home from "./Pages/HomePage/Home";
-import LoginPage from "./Pages/LoginPage/Login";
-import RegisterPage from "./Pages/RegisterPage/Register";
-import ProfilePage from "./Pages/ProfilePage/Profile";
-import PostPage from "./Pages/PostPage/PostPage";
-import SettingsPage from "./Pages/SettingsPage/SettingsPage";
-import EmailConfirmationPage from "./Pages/EmailConfirmationPage/EmailConfirmationPage";
-import ResetPasswordPage from "./Pages/ResetPasswordPage/ResetPasswordPage";
-import ResetPasswordSendEmailPage from "./Pages/ResetPasswordSendEmailPage/ResetPasswordSendEmailPage";
-import SuggestionsPage from "./Pages/SuggestionsPage/SuggestionsPage";
+import Spinner from "./Ui/Spinner";
+const Home = lazy(() => import("./Pages/HomePage/Home"));
+const LoginPage = lazy(() => import("./Pages/LoginPage/Login"));
+const RegisterPage = lazy(() => import("./Pages/RegisterPage/Register"));
+const ProfilePage = lazy(() => import("./Pages/ProfilePage/Profile"));
+const PostPage = lazy(() => import("./Pages/PostPage/PostPage"));
+const SettingsPage = lazy(() => import("./Pages/SettingsPage/SettingsPage"));
+const EmailConfirmationPage = lazy(
+  () => import("./Pages/EmailConfirmationPage/EmailConfirmationPage")
+);
+const ResetPasswordPage = lazy(
+  () => import("./Pages/ResetPasswordPage/ResetPasswordPage")
+);
+const ResetPasswordSendEmailPage = lazy(
+  () => import("./Pages/ResetPasswordSendEmailPage/ResetPasswordSendEmailPage")
+);
+const SuggestionsPage = lazy(
+  () => import("./Pages/SuggestionsPage/SuggestionsPage")
+);
 
 function App() {
   return (
-    <Routes>
-      <Route element={<PersistLogin />}>
-        <Route element={<RequireAuth />}>
-          <Route index element={<Home />} />
-          <Route path="profile/edit" element={<SettingsPage />} />
-          <Route path="suggestions" element={<SuggestionsPage />} />
+    <Suspense fallback={<Spinner size={"big"} />}>
+      <Routes>
+        <Route element={<PersistLogin />}>
+          <Route element={<RequireAuth />}>
+            <Route index element={<Home />} />
+            <Route path="profile/edit" element={<SettingsPage />} />
+            <Route path="suggestions" element={<SuggestionsPage />} />
+          </Route>
+          <Route path="register" element={<RegisterPage />} />
+          <Route
+            path="confirm-email/:confirmationCode"
+            element={<EmailConfirmationPage />}
+          />
+          <Route path="login" element={<LoginPage />} />
+          <Route
+            path="reset-password"
+            element={<ResetPasswordSendEmailPage />}
+          />
+          <Route path="reset-password/:token" element={<ResetPasswordPage />} />
+          <Route path="users/:username" element={<ProfilePage />} />
+          <Route path="posts/:postId" element={<PostPage />} />
         </Route>
-        <Route path="register" element={<RegisterPage />} />
         <Route
-          path="confirm-email/:confirmationCode"
-          element={<EmailConfirmationPage />}
+          path="*"
+          element={
+            <main style={{ padding: "1rem" }}>
+              <p>There's nothing here!</p>
+            </main>
+          }
         />
-        <Route path="login" element={<LoginPage />} />
-        <Route path="reset-password" element={<ResetPasswordSendEmailPage />} />
-        <Route path="reset-password/:token" element={<ResetPasswordPage />} />
-        <Route path="users/:username" element={<ProfilePage />} />
-        <Route path="posts/:postId" element={<PostPage />} />
-      </Route>
-      <Route
-        path="*"
-        element={
-          <main style={{ padding: "1rem" }}>
-            <p>There's nothing here!</p>
-          </main>
-        }
-      />
-    </Routes>
+      </Routes>
+    </Suspense>
   );
 }
 
--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -1,15 +1,17 @@
-import { memo } from "react";
+import { lazy, memo, Suspense } from "react";
 import Logo from "../Logo/Logo";
 import SearchBar from "../SearchBar/SearchBar";
 import ProfileBtn from "./ProfileBtn/ProfileBtn";
 import "./NavBar.scss";
-import CreatePostBtn from "./CreatePostBtn/CreatePostBtn";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { RootState } from "../../Redux/Store";
-import CreatePostModal from "../CreatePostModal/CreatePostModal";
 import { useContext } from "react";
 import { NavBarContext } from "../../Context/NavBarContext";
+import CreatePostBtn from "./CreatePostBtn/CreatePostBtn";
+const CreatePostModal = lazy(
+  () => import("../CreatePostModal/CreatePostModal")
+);
 const NavBar = () => {
   const authState = useSelector((state: RootState) => state.auth);
   const { isCreatePostModalActive } = useContext(NavBarContext);
@@ -19,7 +21,6 @@ const NavBar = () => {
         <Link to="/">
           <Logo />
         </Link>
-        <SearchBar />
         <div className="navBar__buttonsAndLinks">
           {authState.user?.id ? (
             <>
@@ -38,7 +39,11 @@ const NavBar = () => {
           )}
         </div>
       </div>
-      {isCreatePostModalActive && <CreatePostModal />}
+      {isCreatePostModalActive && (
+        <Suspense fallback={<p></p>}>
+          <CreatePostModal />
+        </Suspense>
+      )}
     </nav>
   );
 };

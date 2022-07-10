859eef79536516af7549ebf37ce0c8ff1267931a
Alex Daniel <alexey9021@gmail.com>
2022-07-11 00:45:30 +0300

    Update PostModalCtrl

--- src/Components/PostModal/PostModalComponents/PostModalCtrl.tsx
+++ src/Components/PostModal/PostModalComponents/PostModalCtrl.tsx
@@ -1,23 +1,32 @@
-import useRedux from '../../../Hooks/useRedux';
-import { resetPostState } from '../../../Redux/Post';
+import useRedux from "../../../Hooks/useRedux";
+import { resetPostState } from "../../../Redux/Post";
 
 const PostModalCtrl = ({
   direction,
   handleNavigatePosts,
+  lastFocusableElementRef,
 }: {
-  direction: 'prev' | 'next';
-  handleNavigatePosts: (direction: 'prev' | 'next') => void;
+  direction: "prev" | "next";
+  handleNavigatePosts: (direction: "prev" | "next") => void;
+  lastFocusableElementRef?: any;
 }) => {
   const { dispatch } = useRedux();
   return (
     <button
       className={`postModal__ctrl ctrl--${direction}`}
+      id={`postModal-${direction}Btn`}
       onClick={() => {
         dispatch(resetPostState());
         handleNavigatePosts(direction);
       }}
+      ref={lastFocusableElementRef}
+      aria-label={direction === "prev" ? "Previous post" : "Next post"}
     >
-      <i className={`fa-solid fa-chevron-${direction === 'prev' ? 'left' : 'right'}`}></i>
+      <i
+        className={`fa-solid fa-chevron-${
+          direction === "prev" ? "left" : "right"
+        }`}
+      ></i>
     </button>
   );
 };

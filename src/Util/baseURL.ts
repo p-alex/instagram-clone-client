ff329a964de253a814d6edadf6b07cc7c8f9759c
p-alex <alexey9021@gmail.com>
2022-12-03 20:24:03 +0200

    Changed server base url

--- src/Util/baseURL.ts
+++ src/Util/baseURL.ts
@@ -1,4 +1,4 @@
 export const BASE_URL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:5001/graphql'
-    : 'https://bubble-9021.herokuapp.com/graphql';
+    : 'https://web-production-05a0.up.railway.app/graphql';

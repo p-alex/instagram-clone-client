a4c36c48215634ded5b5777c358ee3d55f605bdb
Alex Daniel <alexey9021@gmail.com>
2023-02-20 19:02:06 +0200

    Update baseURL.ts

--- src/Util/baseURL.ts
+++ src/Util/baseURL.ts
@@ -1,4 +1,4 @@
 export const BASE_URL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:5001/graphql'
-    : 'https://web-production-05a0.up.railway.app/graphql';
+    : 'instagram-clone-p-alex.up.railway.app/graphql';

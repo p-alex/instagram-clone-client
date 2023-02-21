3834eba60f48db8152728c9ac7bfb1fd6ece250c
Alex Daniel <alexey9021@gmail.com>
2023-02-21 21:15:19 +0200

    Update baseURL.ts

--- src/Util/baseURL.ts
+++ src/Util/baseURL.ts
@@ -1,4 +1,4 @@
 export const BASE_URL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:5001/graphql'
-    : 'https://bubble-server-p-alex.up.railway.app/graphql';
+    : 'https://bubble-server.pistolalex.com/graphql';
